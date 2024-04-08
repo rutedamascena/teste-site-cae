a82aef8ba086507eccfb48a6768acf0b4918392a
Rute Damascena <damascenarute@gmail.com>
2024-04-07 22:46:38 -0300

    onze

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for
 from scraping import buscar_noticias_bahia_noticias
+import requests
 
 app = Flask(__name__)
 
