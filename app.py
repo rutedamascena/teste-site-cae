23a8f881b42f4937ae3f5c0ce2bd5cdf27cce2cb
Rute Damascena <damascenarute@gmail.com>
2024-04-08 15:12:08 -0300

    25

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
-from scraping import gerar_dados_html  
+from scraping import dados_html 
+from scraping import gerar_dados_html 
 
 app = Flask(__name__, template_folder='templates')
 
