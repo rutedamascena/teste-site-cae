b0ca51d53c2f3dddadb148c405aea4b4292c6a57
Rute Damascena <damascenarute@gmail.com>
2024-04-08 14:04:53 -0300

    23

--- scraping.py
+++ scraping.py
@@ -2,26 +2,26 @@ import requests
 import bs4
 import pandas as pd
 
-def buscar_noticias_bahia_noticias(url):
+def gerar_dados_html(url):
     requisicao = requests.get(url)
-    html = requisicao.content
-    sopa = bs4.BeautifulSoup(html, 'html.parser')
-    
-    clipping_bahianoticias = sopa.findAll('div', {'class': 'sc-3c754a49-0 edpZTL'})
-    titulos = sopa.find_all('h3', {'class': 'sc-3c754a49-2 iihoil'})
-    
-    lista_titulos = []
-    for titulo in titulos:
-        lista_titulos.append(titulo.text.strip('...'))
-    
-    manchetes_link = []
-    for noticia, titulo in zip(clipping_bahianoticias, titulos):
-        link_ = 'https://www.bahianoticias.com.br' + noticia.find('a').get('href')
-        titulo_ = titulo.text.strip('...')
-        manchetes_link.append([titulo_, link_])
-    
-    bahia_noticias = pd.DataFrame(manchetes_link, columns=['Título', 'Link'])
-    bahia_noticias['veículo'] = 'BAHIA NOTÍCIAS'
-    
-    dados_html = bahia_noticias.to_html(index=False)  # Convertendo DataFrame para HTML
-    return dados_html
+    if requisicao.status_code == 200:
+        html = requisicao.content
+        sopa = bs4.BeautifulSoup(html, 'html.parser')
+        clipping_bahianoticias = sopa.findAll('div', {'class': 'sc-3c754a49-0 edpZTL'})
+        titulos = []
+        links = []
+        for noticia in clipping_bahianoticias:
+            titulo = noticia.find('h3', {'class': 'sc-3c754a49-2 iihoil'}).text.strip()
+            link = 'https://www.bahianoticias.com.br' + noticia.find('a').get('href')
+            titulos.append(titulo)
+            links.append(link)
+        dados_noticias = pd.DataFrame({'Título': titulos, 'Link': links})
+        dados_noticias['Veículo'] = 'Bahia Notícias'
+        html_formatado = dados_noticias.to_html(index=False)
+        return html_formatado
+    else:
+        return None
+
+url = 'https://www.bahianoticias.com.br/pesquisa/petrobras'
+dados_html = gerar_dados_html(url)
+print(dados_html)
