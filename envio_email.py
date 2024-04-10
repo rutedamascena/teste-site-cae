fcf1bf72cb5d0c46247d4c0f524e2493d0ea0acb
Rute Damascena <damascenarute@gmail.com>
2024-04-10 12:47:08 -0300

    42

--- envio_email.py
+++ envio_email.py
@@ -3,15 +3,15 @@ import smtplib
 from email.mime.multipart import MIMEMultipart 
 from email.mime.text import MIMEText
 from scraping import dados_html
-#import dotenv
-#import os SOCORRO
+from dotenv import load_dotenv 
+import os
 
 
 def enviar_email_com_html(html):
     smtp_server = "smtp-relay.brevo.com"
     port = 587
     email = "damascenarute@gmail.com"
-    password =  "xsmtpsib-968472d0272c0aa6d32d0c58ef790c4b58a21dfad3e90d739e278201745bfb19-L9VBq8n7cFANaTRC"
+    password = os.getenv ('CHAVE_EMAIL')
     remetente = "damascenarute@gmail.com"
     destinatario_email = "anadirdamascena@gmail.com"
 
